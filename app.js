6b53d8a2f99e02fa1a214c6e03c8b67a983517a1
Cesar Preza <cpreza93@yahoo.com>
2020-09-23 22:16:36 -0700

    Create computer hand variables

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function playerChoice() {
     const playerChoice = document.getElementById('player-choice');
     const rock = document.getElementById('rock-btn');
     const paper = document.getElementById('paper-btn');
     const scissors = document.getElementById('scissors-btn');
-
     rock.addEventListener('click', () => {
         playerChoice.src = "images/rock.png";
-    })
-
+    });
     paper.addEventListener('click', () => {
         playerChoice.src = 'images/paper.png';
-    })
+    });
     scissors.addEventListener('click', () => {
         playerChoice.src = 'images/scissors.png';
-    })
+    });
 }
+
+
+const options = document.querySelectorAll('.options-btn button');
+const randomPick = ['rock', 'paper', 'scissors'];
+const randomNumber = Math.floor(Math.random() * 3);
+const choice = randomPick[randomNumber];
+
+console.log(choice);
+
 
 playerChoice();
